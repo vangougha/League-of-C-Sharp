d78ad5a7cc099f31cbe4a8382da556b66ec9d727
Umut <umtyrdql@gmail.com>
2024-08-02 22:05:19 +0300

    Update

--- LeagueOfCSharp/LeagueOfCSharp/Program.cs
+++ LeagueOfCSharp/LeagueOfCSharp/Program.cs
@@ -1,42 +1,44 @@
 ﻿using System;
 using Camille.Enums;
 using Camille.RiotGames;
+using Camille.RiotGames.SummonerV4;
+using Camille.RiotGames.LeagueV4;
 
 
 namespace LeagueOfCSharp
 {
     class Program
     {
-
         public static void GetTopChampions()
         {
-            var riotApi = RiotGamesApi.NewInstance("RGAPI-e67364f6-c6bf-4a4e-b5ef-b6969130d0f5");
+            var riotApi = RiotGamesApi.NewInstance("RGAPI-cfdf3190-6fee-41f1-8a61-51bbc5b71b45");
 
+            // Assuming both API calls return types that share a common interface or base class
             var summoners = new[]
             {
-                    riotApi.SummonerV4().GetBySummonerId(PlatformRoute.RU,"communistcomrade#CCCP")
-
-        };
+                riotApi.AccountV1().GetByRiotId(RegionalRoute.EUROPE, "communistcomrade", "CCCP"),
+            };
             foreach (var summoner in summoners)
             {
-                Console.WriteLine($"Summoner Id is : {summoner.Id} ,\n and Summoner Level is  {summoner.SummonerLevel}");
+                Console.WriteLine($"Summoner Id is : {summoner.GameName},\n ");
                 var masteries = riotApi.ChampionMasteryV4().GetAllChampionMasteriesByPUUID(PlatformRoute.RU, summoner.Puuid);
                 for (int i = 0; i < 10; i++)
                 {
                     var mastery = masteries[i];
                     var champ = (Champion)mastery.ChampionId;
-                    Console.WriteLine("{0,3}) {1,-16} {2,10:N0} ({3})", i + 1, champ.ToString(),
-              mastery.ChampionPoints, mastery.ChampionLevel);
+                    var lastPlayTime = (Champion)mastery.LastPlayTime;
+                    Console.WriteLine("{0,3}) {1,-16} {2,10:N0} ({3})", i + 1, champ.ToString(), mastery.ChampionPoints, mastery.ChampionLevel);
+                    Console.WriteLine($"Last play time : {lastPlayTime} ");
                 }
                 Console.WriteLine();
             }
-
         }
+
+
 
         static void Main(string[] args)
         {
             GetTopChampions();
         }
-
     }
 }
